c72a5fa046e0dd8fe2213e993304d644d1d23528
kanva001 <94487704+kanva001@users.noreply.github.com>
2024-05-23 08:33:55 -0400

    Project created and added

---
+++ main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+service = Service(executable_path="chromedriver.exe")
+
+driver = webdriver.Chrome(service=service)  # Pass the 'service' object you created
+
+driver.get("https://google.com")
+
+input_element = driver.find_element(By.CLASS_NAME,"gLFyf").send_keys("hosanna official" + Keys.ENTER)
+
+time.sleep(10)
+
+driver.quit()
